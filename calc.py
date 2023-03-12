585a7cfb6b8279597d33c2536c52c9191c253e06
Miguel <115882736+staxhinho@users.noreply.github.com>
2023-03-12 17:21:18 +0000

    Main Upload

---
+++ calc.py
@@ -0,0 +1,4 @@
+expressao_matematica = input("Digita uma expressão matemática:")
+resultado = eval(expressao_matematica)
+print(f"O resultado é {resultado}")
+input("Pressione Enter para encerrar!")
